0ae94867a0caf1822c2abb8d8bed562ae0f07316
cjp34 <carlparm@gmail.com>
2020-02-03 16:13:36 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,52 @@
+require 'pry'
+
 class Artist
 
   attr_reader :name, :years_experience
+  @@all = []
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select do |paintings|
+      paintings.artist == self
+    end
+  end
+
+  def galleries
+    Painting.all.map do |paintings|
+      if paintings.artist == self
+        paintings.gallery
+      end
+    end
+  end
+
+  def cities
+    self.galleries.map do |galleries|
+      galleries.city
+    end
+  end 
+
+  def self.total_experience
+    self.all.select do |artist|
+      artist.years_experience
+    end.sum
+  end
+
+  def self.most_prolific
+
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
   end
 
 end
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,34 @@
 class Gallery
 
   attr_reader :name, :city
+  @@all = []
 
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select do |paintings|
+      paintings.gallery == self
+    end
+  end
+
+  def artists
+    self.paintings.map do |paintings|
+      paintings.artist
+    end
+  end
+
+  def artist_names
+    self.artists.map do |artists|
+      artists.name
+    end
   end
 
 end
