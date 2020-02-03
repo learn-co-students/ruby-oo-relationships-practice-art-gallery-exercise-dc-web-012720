27178e6ca8937a2577c14fd6173f9e6706115cb2
bumbot <pkim1295@gmail.com>
2020-02-03 17:20:43 -0500

    Done.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,9 +2,32 @@ class Gallery
 
   attr_reader :name, :city
 
+  @@all = []
   def initialize(name, city)
     @name = name
     @city = city
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select{|paint| paint.gallery == self}
+  end
+
+  def artists
+    self.paintings.map{|paint| paint.artist}.uniq
+  end
+
+  def artist_names
+    self.artists.map{|artist| artist.name}
+  end
+
+  def most_expensive_painting
+    self.paintings.max_by {|painting| painting.price}
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,8 +1,24 @@
 require_relative '../config/environment.rb'
 
+ross = Artist.new("Bob Ross", 20)
+gogh = Artist.new("Vincent Van Gogh", 40)
+vinci = Artist.new("Leonardo Da Vinci", 35)
+
+louvre = Gallery.new("Louvre Museum", "Paris")
+gugenheim = Gallery.new("Gugenheim Museum", "NYC")
+
+lisa = Painting.new("Mona Lisa", 5000000, vinci, louvre)
+night = Painting.new("The Starry Night", 3500000, gogh, gugenheim)
+portrait = Painting.new("Van Gogh Self Portrait", 10, gogh, louvre)
+waterfall = Painting.new("Alaskan Waterfall", 100000, ross, gugenheim)
+brook = Painting.new("Bubbling Brook", 100000, ross, gugenheim)
 
 
 
-binding.pry
+
+
+
+
+Pry.start
 
 puts "Bob Ross rules."
