8e1edc26363e47dc45cccadd2682058619c8d45d
vplacido <valentinplacido25@gmail.com>
2020-02-03 21:44:21 -0500

    finished

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,9 +2,36 @@ class Gallery
 
   attr_reader :name, :city
 
+  @@all = []
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
   end
 
+  def self.all 
+    @@all
+  end
+
+  def paintings
+    Painting.all.select{|painting| painting.gallery == self}
+  end
+
+  def artists
+    paintings.map{|painting| painting.artist}
+  end
+
+  def artist_names
+    artist.map{|artist| artist.name}
+  end
+  
+  def most_expensive_painting
+    highestTotal = paintings[0]
+    paintings.each{|painting| 
+      if painting.price > highestTotal.price
+        highestTotal = painting
+      end
+    }
+    highestTotal
+  end 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,5 +1,29 @@
 require_relative '../config/environment.rb'
 
+betterBest = Gallery.new("Top of the Line", "LA")
+bestGallery = Gallery.new("The Best", "Monaco")
+okGallery = Gallery.new("Average", "NYC")
+worstGallery = Gallery.new("The Worst Gallery", "Ohio")
+
+
+jon = Artist.new("Jon Doe", 20)
+jane = Artist.new("Jane Doe", 1)
+
+jonPainting1 = Painting.new("one", 1000, jon, okGallery)
+jonPainting2 = Painting.new("two", 10000, jon, okGallery)
+jonPainting3 = Painting.new("three", 100000, jon, bestGallery)
+jonPainting4 = Painting.new("four", 1000000, jon, betterBest)
+janePainting1 = Painting.new("janeOne", 0, jane, worstGallery)
+janePainting2 = Painting.new("janeTwo", 10, jane, worstGallery)
+janePainting3 = Painting.new("janeThree", 20, jane, worstGallery)
+janePainting4 = Painting.new("janeFour", 20, jane, worstGallery)
+janePainting5 = Painting.new("janeFive", 20, jane, worstGallery)
+janePainting6 = Painting.new("janeSix", 50, jane, okGallery)
+janePainting7 = Painting.new("janeSeven", 5, jane, worstGallery)
+janePainting8 = Painting.new("janeEight", 2, jane, worstGallery)
+janePainting9 = Painting.new("janeNine", 1, jane, worstGallery)
+
+
 
 
 
